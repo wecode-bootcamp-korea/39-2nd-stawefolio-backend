d4cc59868e42a1b7a21682a604cb63941c6bf09e
이명석 <yhkyhk92@gmail.com>
2022-11-28 23:44:45 +0900

    [Add] error handler

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const app = express();
 
 const { appDataSource } = require('./src/models/dataSource');
 const { routes } = require('./src/routes');
+const errorHandler = require('./src/utils/errorHandler');
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(routes);
+app.use(errorHandler.globalErrorHandler());
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
