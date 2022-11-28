46d227410e6a248a8b63a3758e066349da747015
이명석 <yhkyhk92@gmail.com>
2022-11-28 18:24:25 +0900

    초기 개발 환경 세팅

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+const { appDataSource } = require('./src/models/dataSource');
+const { routes } = require('./src/routes');
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(routes);
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+const start = async () => {
+  const PORT = process.env.PORT;
+
+  await appDataSource
+    .initialize()
+    .then(() => {
+      console.log('Data Source has been initialized');
+    })
+    .catch((err) => {
+      console.error('Error during Data Source initialization', err);
+      appDataSource.destroy();
+    });
+
+  app.listen(PORT, () =>
+    console.log(`server is listening on http://localhost:${PORT}`)
+  );
+};
+
+start();
