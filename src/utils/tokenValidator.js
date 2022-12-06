360849d25534bacf57e360cb4b46f978f61e2f0d
이명석 <yhkyhk92@gmail.com>
2022-12-06 15:43:03 +0900

    modify: tokenValidator

--- src/utils/tokenValidator.js
+++ src/utils/tokenValidator.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 const authDao = require('../models/authDao');
 const { customError } = require('../utils/errorHandler');
 
-const tokenValidator = async (jwtToken, next) => {
+const tokenValidator = async (req, res, next) => {
+  const jwtToken = req.header('Authorization');
   const payload = jwt.verify(jwtToken, process.env.JWT_SECRET_KEY);
-
   const user = authDao.getUserByKakaoId(payload.kakaoId);
 
   if (!user) customError('Invalid User', 401);
